878f3fed64f16c7c90b51ca9b7ee26af188593d0
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2020-12-22 23:47:17 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
+from setuptools import setup
 
+setup(name='based',
+      version='0.1.0',
+      description='a base converter, encrypter and compiler',
+      author='Y0N1N1 (gabriel)',
+      license='MIT',
+      packages = ['based'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+      ],
+      install_requires=['math','os'],
+      python_requires='>=3.8',
+      extras_require={
+        'dev': [
+            "math",
+            "os"
+        ],
+      },
+      include_package_data=True)
